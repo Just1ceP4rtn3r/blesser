6e7b07e56aa6742e9dd97167a87110601042341c
just1cep4rtn3r@gmail.com <just1cep4rtn3r@gmail.com>
2023-04-23 22:03:55 +0800

    update

--- src/core.py
+++ src/core.py
@@ -1,25 +1,5 @@
 from SMPStateMachine import SMPStateMachine
 
-SMP_CODE = {
-    0x01: "smp_pairing_req",
-    0x02: "smp_pairing_rsp",
-    0x03: "smp_pairing_confirm",
-    0x04: "smp_pairing_random",
-    0x05: "smp_pairing_failed",
-    0x06: "smp_encrypt_info",
-    0x07: "smp_central_ident",
-    0x08: "smp_ident_info",
-    0x09: "smp_ident_addr_info",
-    0x0a: "smp_signing_info",
-    # Peripheral -> Central; The Security Request command is used by the Peripheral to request that the Central initiates security with the requested security properties, see Section 2.4.6. The Security Request command is defined in Figure 3.17.
-    0x0b: "smp_security_request",
-    # Central -> Peripheral | Peripheral -> Central;
-    0x0c: "smp_public_key",
-    # Central -> Peripheral | Peripheral -> Central;
-    0x0d: "smp_dhkey_check",
-    0x0e: "smp_keypress_notif",
-}
-
 
 #########################
 # SMP socket
