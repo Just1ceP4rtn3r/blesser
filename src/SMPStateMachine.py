ea6fb3bf9d5e50c3d7da5420bcbba4e885154249
just1cep4rtn3r@gmail.com <just1cep4rtn3r@gmail.com>
2023-04-24 17:08:53 +0800

    update

--- src/SMPStateMachine.py
+++ src/SMPStateMachine.py
@@ -13,33 +13,33 @@ class SMPStateMachine(StateMachine):
 
     #### code:0x05 Pairing Failed####
     # reason:0x01; Passkey Entry Failed
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Passkey Entry Failed', value=0x0501)
+    receive_pairing_failed_state_1 = State('Receive Pairing Failed State;Passkey Entry Failed', value=0x0501)
     # reason:0x02; OOB Not Available
-    receive_pairing_failed_state = State('Receive Pairing Failed State;OOB Not Available', value=0x0502)
+    receive_pairing_failed_state_2 = State('Receive Pairing Failed State;OOB Not Available', value=0x0502)
     # reason:0x03; Authentication Requirements
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Authentication Requirements', value=0x0503)
+    receive_pairing_failed_state_3 = State('Receive Pairing Failed State;Authentication Requirements', value=0x0503)
     # reason:0x04; Confirm Value Failed
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Confirm Value Failed', value=0x0504)
+    receive_pairing_failed_state_4 = State('Receive Pairing Failed State;Confirm Value Failed', value=0x0504)
     # reason:0x05; Pairing Not Supported
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Pairing Not Supported', value=0x0505)
+    receive_pairing_failed_state_5 = State('Receive Pairing Failed State;Pairing Not Supported', value=0x0505)
     # reason:0x06; Encryption Key Size
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Encryption Key Size', value=0x0506)
+    receive_pairing_failed_state_6 = State('Receive Pairing Failed State;Encryption Key Size', value=0x0506)
     # reason:0x07; Command Not Supported
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Command Not Supported', value=0x0507)
+    receive_pairing_failed_state_7 = State('Receive Pairing Failed State;Command Not Supported', value=0x0507)
     # reason:0x08; Unspecified Reason
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Unspecified Reason', value=0x0508)
+    receive_pairing_failed_state_8 = State('Receive Pairing Failed State;Unspecified Reason', value=0x0508)
     # reason:0x09; Repeated Attempts
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Repeated Attempts', value=0x0509)
+    receive_pairing_failed_state_9 = State('Receive Pairing Failed State;Repeated Attempts', value=0x0509)
     # reason:0x0a; Invalid Parameters
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Invalid Parameters', value=0x050a)
+    receive_pairing_failed_state_10 = State('Receive Pairing Failed State;Invalid Parameters', value=0x050a)
     # reason:0x0b; DHKey Check Failed
-    receive_pairing_failed_state = State('Receive Pairing Failed State;DHKey Check Failed', value=0x050b)
+    receive_pairing_failed_state_11 = State('Receive Pairing Failed State;DHKey Check Failed', value=0x050b)
     # reason:0x0c; Numeric Comparison Failed
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Numeric Comparison Failed', value=0x050c)
+    receive_pairing_failed_state_12 = State('Receive Pairing Failed State;Numeric Comparison Failed', value=0x050c)
     # reason:0x0d; BR/EDR Pairing In Progress
-    receive_pairing_failed_state = State('Receive Pairing Failed State;BR/EDR Pairing In Progress', value=0x050d)
+    receive_pairing_failed_state_13 = State('Receive Pairing Failed State;BR/EDR Pairing In Progress', value=0x050d)
     # reason:0x0e; Cross Transport Key Derivation/Generation Not Allowed
-    receive_pairing_failed_state = State('Receive Pairing Failed State;Cross Transport Key Derivation/Generation Not Allowed',
+    receive_pairing_failed_state_14 = State('Receive Pairing Failed State;Cross Transport Key Derivation/Generation Not Allowed',
                                          value=0x050e)
 
     #### code:0x02 Pairing Response ####
