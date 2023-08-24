ca9af66e89888b9ca641b1e5f29071311c5ef216
zy <zr4in@qq.com>
2023-08-24 19:29:04 +0800

    add Instruction.h

--- BlesserBackend/Include/Instruction.h
+++ BlesserBackend/Include/Instruction.h
@@ -1,5 +1,14 @@
-struct INST
-{
-    char * CMD_str;
+#define uint8_t char
+#define MAX_MUTATION_COUNT 20
+#define MAX_MUTATION_VALUES_LEN 100
 
+struct mutation{
+    uint8_t cmd_id;
+    uint8_t filed_id;
+}
+
+struct Instruction{
+    uint8_t mutation_count;
+    struct mutation mutations[MAX_MUTATION_COUNT];
+    uint8_t mutation_values[MAX_MUTATION_VALUES_LEN];
 };
