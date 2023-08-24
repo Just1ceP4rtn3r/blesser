3a6d206c3a2eede67c186ca9d7c84ccc51adb013
zy <zr4in@qq.com>
2023-08-24 20:30:41 +0800

    add thread_blesser_backend for main

--- BlesserBackend/src/main.c
+++ BlesserBackend/src/main.c
@@ -45,45 +45,37 @@ static void start_scan(void);
 
 static struct bt_conn *default_conn;
 
-// struct smp_recv_handler{
-// 	uint8_t  (*func)(uint8_t *packet, int len);
-// };
 uint8_t uart_send_func(uint8_t *packet, int len){
 	int err = uart_tx(uart, packet, len, SYS_FOREVER_US);
 	return err;
 }
 K_MUTEX_DEFINE(app_buf_mutex);
+
 // flag to indicate if app_buf have data
 bool recv_flag = false;
 // len to indicate the length of the data in app_buf
 int recv_len = 0;
-void thread_send_smp(void){
-    while(true){
-        k_mutex_lock(&app_buf_mutex, K_FOREVER);
-        if(recv_flag){
-			// data in app_buf, thus send
-            if(default_conn){
-				int i;
-                printk("Start of app_buf\n");
-                //bt_conn_set_security_mine(default_conn, BT_SECURITY_L4, (int)app_buf[0]);
-				for(i=0; i < recv_len; i++){
-					printk("%x ", app_buf[i]);
-				}
-				printk("\nend app_buf\nStart send over conn\n");
 
-				// bt_conn_send_smp_packet(default_conn, app_buf, recv_len, 1);
-            } else {
-                printk("Conn NULL\n");
-            }
-            recv_flag = 0;
-        } else {
-            // no data in app_buf to send, thus do nothing
-        }
-        k_mutex_unlock(&app_buf_mutex);
+void thread_blesser_backend(void){
+	while (true)
+	{
+		k_mutex_lock(&app_buf_mutex, K_FOREVER);
+		if(recv_flag){
+			if(default_conn){
+				struct BlesserInstruction frontendInstruction;
+				parse(&frontendInstruction, app_buf, recv_len);
+				// do fuzz with instruction
+				recv_flag = 0;
+			} else {
+				printk("[ERROR]: connection is NULL\n");
+			}
+		}
+		k_mutex_unlock(&app_buf_mutex);
         k_msleep(100);
-    }
+	}
+	
 }
-K_THREAD_DEFINE(thread_send_smp_id, 1024, thread_send_smp, NULL, NULL, NULL, 10, 0, 0);
+K_THREAD_DEFINE(thread_blesser_backend_id, 1024, thread_blesser_backend, NULL, NULL, NULL, 10, 0, 0);
 static void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data)
 {
 	switch (evt->type) {
@@ -97,21 +89,11 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
         uart_rx_disable(uart);
         k_mutex_unlock(&app_buf_mutex);
 	    break;
+
 	case UART_RX_DISABLED:
-        // k_mutex_lock(&app_buf_mutex, K_FOREVER);
-        // while(recv_flag)
-        // {
-		// 	k_msleep(100);
-        //     k_mutex_unlock(&app_buf_mutex);
-        // } else {
-        //     uart_rx_enable(dev, rx_buf, sizeof rx_buf, RECEIVE_TIMEOUT);
-        //     k_mutex_unlock(&app_buf_mutex);
-        // }
-		// k_mutex_lock(&app_buf_mutex, K_FOREVER);
-        // recv_flag = 0;
     	uart_rx_enable(uart, rx_buf, sizeof rx_buf, RECEIVE_TIMEOUT);
-        // k_mutex_unlock(&app_buf_mutex);
 		break;
+
 	default:
 		break;
 	}
@@ -137,14 +119,9 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 	printk("Device found: %s (RSSI %d)\n", addr_str, rssi);
 
 	/* connect only to devices in close proximity */
-	// if (rssi < -70) {
-	// 	return;
-	// }F4:5A:DE:3A:CC:5F (random)
-	if (strcmp(addr_str, "F4:5A:DE:3A:CC:5F (random)"))
-    {
-        printk("Not same\n");
-        return;
-    }
+	if (rssi < -70) {
+		return;
+	}
 
 	if (bt_le_scan_stop()) {
 		return;
@@ -219,16 +196,6 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.connected = connected,
 	.disconnected = disconnected,
 };
-void complete(struct bt_conn *conn, bool bonded){
-	printk("auth complete\n");
-}
-void fail(struct bt_conn *conn, enum bt_security_err reason){
-	printk("auth failed\n");
-}
-struct bt_conn_auth_info_cb auth_info_cb = {
-	.pairing_complete = complete,
-	.pairing_failed = fail,
-};
 
 void main(void)
 {
@@ -251,7 +218,7 @@ void main(void)
 	}
 
 	printk("Bluetooth initialized\n");
-	bt_conn_auth_info_cb_register(&auth_info_cb);
+
     uart_rx_enable(uart, rx_buf, sizeof rx_buf, RECEIVE_TIMEOUT);
 	start_scan();
 }
