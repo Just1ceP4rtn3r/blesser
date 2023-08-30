0de62360b303de1ccf8ce1a2eeafd5fdcdc67a11
MQTTactic <MQTTactic@gmail.com>
2023-08-30 17:30:13 +0800

    update

--- test/serial_test.py
+++ test/serial_test.py
@@ -3,5 +3,5 @@ import serial.tools.list_ports
 
 ser = serial.Serial("COM3", 115200)
 a = 0
-while (input(a)):
+while(a = input()):
     ser.write(bytes.fromhex("0400000102030405060708090a0b0c0d0e0f0100ff"))
