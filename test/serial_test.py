c76907e279224acce639d7d54ae3d7457802120c
just1cep4rtn3r@gmail.com <-global>
2023-08-31 09:37:40 +0800

    update

--- test/serial_test.py
+++ test/serial_test.py
@@ -2,6 +2,21 @@ import serial
 import serial.tools.list_ports
 
 ser = serial.Serial("COM3", 115200)
-a = 0
-while(a = input()):
+
+
+def send():
+    a = input("byte: ")
     ser.write(bytes.fromhex("0400000102030405060708090a0b0c0d0e0f0100ff"))
+
+
+def recv():
+    com_input = b''
+    while True:
+        com_input = ser.read_all()
+        if (com_input != b""):
+            print(com_input)
+
+
+if __name__ == "__main__":
+    send()
+    recv()
